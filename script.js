bc36e852c8192d90cd67be92091ac7b78190bfbc
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2024-09-11 18:25:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,15 +21,81 @@ const openFavoritesButton = document.getElementById('open-favorites');
 // Массив избранных фильмов
 let favoriteMovies = [];
 
-// Функция для получения фильмов
+// Функция для получения фильмов, сериалов и мультфильмов
 function fetchMovies(query) {
-    fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=ru-RU&query=${query}`)
-        .then(response => response.json())
-        .then(data => {
-            displayMovies(data.results, newReleasesGrid);
-        });
+    Promise.all([
+        fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=ru-RU&query=${query}`),
+        fetch(`${BASE_URL}/search/tv?api_key=${API_KEY}&language=ru-RU&query=${query}`)
+    ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(([movieData, tvData]) => {
+        const movies = movieData.results.map(item => ({...item, media_type: 'movie'}));
+        const tvShows = tvData.results.map(item => ({...item, media_type: 'tv', title: item.name}));
+        const allResults = [...movies, ...tvShows].sort((a, b) => b.popularity - a.popularity);
+        displayMovies(allResults, newReleasesGrid);
+    });
+}
+// ... (предыдущий код)
+
+const sliderContainer = document.querySelector('.slider-container');
+const prevButton = document.querySelector('.slider-button.prev');
+const nextButton = document.querySelector('.slider-button.next');
+
+let currentIndex = 0;
+
+function updateSlider() {
+  const movieWidth = 210; // Ширина фильма + отступ
+  sliderContainer.style.transform = `translateX(${-currentIndex * movieWidth}px)`;
 }
 
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentIndex < sliderContainer.children.length - 5) { // Показываем по 5 фильмов
+    currentIndex++;
+    updateSlider();
+  }
+});
+
+// Обновляем функцию displayMovies
+function displayMovies(movies, container) {
+  container.innerHTML = '';
+  movies.forEach(movie => {
+    const movieTile = createMovieTile(movie);
+    container.appendChild(movieTile);
+  });
+  currentIndex = 0;
+  updateSlider();
+}
+
+// Обновляем функцию createMovieTile
+function createMovieTile(movie) {
+  const movieTile = document.createElement('div');
+  movieTile.className = 'movie-tile';
+
+  const movieImage = document.createElement('img');
+  movieImage.src = `${IMG_URL}${movie.poster_path}`;
+  movieImage.alt = getTitle(movie);
+
+  const movieTitleElem = document.createElement('h3');
+  movieTitleElem.innerText = getTitle(movie);
+
+  const movieRating = document.createElement('div');
+  movieRating.className = 'rating';
+  movieRating.innerText = movie.vote_average.toFixed(1);
+
+  movieTile.appendChild(movieImage);
+  movieTile.appendChild(movieTitleElem);
+  movieTile.appendChild(movieRating);
+
+  movieTile.onclick = () => showMovieDetailsFromTile(movie);
+  return movieTile;
+}
 // Функция для получения новинок
 function fetchNewReleases() {
     fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=ru-RU`)
@@ -48,6 +114,11 @@ function displayMovies(movies, container) {
     });
 }
 
+// Вспомогательная функция для получения правильного заголовка
+function getTitle(item) {
+    return item.title || item.name;
+}
+
 // Функция для создания плитки фильма
 function createMovieTile(movie) {
     const movieTile = document.createElement('div');
@@ -55,10 +126,10 @@ function createMovieTile(movie) {
 
     const movieImage = document.createElement('img');
     movieImage.src = `${IMG_URL}${movie.poster_path}`;
-    movieImage.alt = movie.title;
+    movieImage.alt = getTitle(movie);
 
     const movieTitleElem = document.createElement('h3');
-    movieTitleElem.innerText = movie.title;
+    movieTitleElem.innerText = getTitle(movie);
 
     const movieRating = document.createElement('div');
     movieRating.className = 'rating';
@@ -71,13 +142,13 @@ function createMovieTile(movie) {
     movieTile.appendChild(movieTitleElem);
     movieTile.appendChild(movieRating);
 
-    movieTile.onclick = () => showMovieDetailsFromTile(movie); // Отображение деталей фильма при клике на плитку
+    movieTile.onclick = () => showMovieDetailsFromTile(movie);
     return movieTile;
 }
 
 // Функция для получения трейлеров фильма
-function fetchMovieTrailer(movieId) {
-    return fetch(`${BASE_URL}/movie/${movieId}/videos?api_key=${API_KEY}&language=ru-RU`)
+function fetchMovieTrailer(movieId, mediaType = 'movie') {
+    return fetch(`${BASE_URL}/${mediaType}/${movieId}/videos?api_key=${API_KEY}&language=ru-RU`)
         .then(response => response.json())
         .then(data => {
             const trailer = data.results.find(video => video.type === 'Trailer');
@@ -88,11 +159,11 @@ function fetchMovieTrailer(movieId) {
 // Функция для отображения деталей фильма с плеером Kinobox
 function showMovieDetails(movie) {
     moviePoster.src = `${IMG_URL}${movie.poster_path}`;
-    movieTitle.innerText = movie.title;
+    movieTitle.innerText = getTitle(movie);
     movieDescription.innerText = movie.overview;
 
     // Получение трейлера
-    fetchMovieTrailer(movie.id).then(trailerKey => {
+    fetchMovieTrailer(movie.id, movie.media_type).then(trailerKey => {
         movieTrailer.src = trailerKey ? `https://www.youtube.com/embed/${trailerKey}` : '';
     });
 
@@ -119,23 +190,23 @@ function showMovieDetails(movie) {
 
     // Установка текста и обработчика для кнопки "Добавить в избранное"
     addToFavoritesButton.innerText = favoriteMovies.some(fav => fav.id === movie.id) ? 'В избранном' : 'Добавить в избранное';
-    addToFavoritesButton.onclick = () => toggleFavorite(movie); // Установка обработчика для кнопки избранного
+    addToFavoritesButton.onclick = () => toggleFavorite(movie);
 
     movieDetailModal.style.display = 'block';
 }
 
 // Функция для получения подробностей о фильме
-function fetchMovieDetails(movieId) {
-    fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=ru-RU`)
+function fetchMovieDetails(movieId, mediaType = 'movie') {
+    fetch(`${BASE_URL}/${mediaType}/${movieId}?api_key=${API_KEY}&language=ru-RU`)
         .then(response => response.json())
         .then(data => {
-            showMovieDetails(data);
+            showMovieDetails({...data, media_type: mediaType});
         });
 }
 
 // Функция для показа деталей фильма из плитки
 function showMovieDetailsFromTile(movie) {
-    fetchMovieDetails(movie.id);
+    fetchMovieDetails(movie.id, movie.media_type);
 }
 
 // Функция для переключения избранного
